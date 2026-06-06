de704d5b22b54e8d8e4586635b1ce1a6bf337aa4
anuragmis2022-del <anuragmis2022@gmail.com>
2026-06-06 18:50:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 const getSumBtn = document.createElement("button");
+const table = document.querySelector("table");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+
+	const price=document.querySelectorAll(".price");
+  let sum=0
+
+  for (let item of price )
+  {
+    sum+=Number(item.innerText)
+    
+  }
+const td = document.createElement("td");
+ const tr = document.createElement("tr");
+td.innerText=sum
+
+
+ tr.append(td)
+ table.append(tr)
   
-};
+
+}
 
 getSumBtn.addEventListener("click", getSum);
 
